523fee171fc1f52d61912d21d997941e32e16d99
selam lemma <oneiconforlife26@gmail.com>
2020-08-29 00:55:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,8 +44,8 @@ def monopoly_with_third_tier
       :shortline=>
       {
         "mortgage_value"=>"$800"
-  },
-}
+  }
+},
   :rent_in_dollars=>{
       :one_piece_owned=>25,
       :two_pieces_owned=>50,
